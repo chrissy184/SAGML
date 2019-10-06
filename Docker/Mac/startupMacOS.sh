20d61f3a227ba2aa32670a421b58b67e3420d6cb
Peeyush pashine <peeyushpashine@gmail.com>
2019-10-07 01:07:25 +0530

    updating the name of file.

--- Docker/Mac/startupMacOS.sh
+++ Docker/Mac/startupMacOS.sh
@@ -4,7 +4,7 @@ cp ZMODMacOS.sh ~/.zmod/
 cp .env ~/.zmod/
 sed -i '' -e "/alias ZMOD=/d" ~/.bashrc 
 sed -i '' -e "/alias zmod=/d" ~/.bashrc
-echo alias ZMOD="~/.zmod/ZMOD.sh" >> ~/.bashrc
-echo alias zmod="~/.zmod/ZMOD.sh" >> ~/.bashrc
-chmod +x ~/.zmod/ZMOD.sh
+echo alias ZMOD="~/.zmod/ZMODMacOS.sh" >> ~/.bashrc
+echo alias zmod="~/.zmod/ZMODMacOS.sh" >> ~/.bashrc
+chmod +x ~/.zmod/ZMODMacOS.sh
 source ~/.bashrc
