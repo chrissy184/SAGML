98fec5567079d5083c3baf0883943c6d3e4369aa
dheeraj <dheeraj.kumar@softwareag.com>
2019-11-29 11:48:06 +0530

    WIP: template added for GET settings

--- zmm/src/App/Controllers/SettingController.cs
+++ zmm/src/App/Controllers/SettingController.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using ZMM.Models.ResponseMessages;
+using ZMM.Models.Payloads;
 
 namespace ZMM.App.Controllers
 {
@@ -21,7 +22,7 @@ namespace ZMM.App.Controllers
         private readonly IWebHostEnvironment _environment;
         readonly ILogger<CodeController> Logger;
         public IConfiguration Configuration { get; }
-        private List<ZSSettingResponse> settingsResponse;       
+        private List<ZSSettingResponse> settingsResponse;
 
         #endregion
 
@@ -30,35 +31,69 @@ namespace ZMM.App.Controllers
         {
             //update 
             _environment = environment ?? throw new ArgumentNullException(nameof(environment));
-            this.Configuration = configuration;            
-            this.Logger = log; 
+            this.Configuration = configuration;
+            this.Logger = log;
         }
         #endregion
-    
+
         #region Add settings
         [HttpPost]
         public async Task<IActionResult> AddSettingAsync()
         {
-            string jsonBody ="";
+            string jsonBody = "", zmodId = "testdk";
+            //read from body
             using (var reader = new StreamReader(Request.Body))
             {
                 var body = await reader.ReadToEndAsync();
                 jsonBody = body.ToString();
             }
+            // zmodId = ZSSettingPayload.GetUserNameOrEmail(HttpContext);
+            //parse
             JObject jObj = JObject.Parse(jsonBody);
-            //jObj.Add("ZmodId", "testdk");
-            JArray jArr =  (JArray) jObj["settings"];
+            JArray jArr = (JArray)jObj["settings"];
             var setList = jArr.ToObject<List<SettingProperty>>();
-
+            //add to payload
             var newRecord = new ZSSettingResponse()
             {
-                ZmodId = "testdk",
+                ZmodId = zmodId,
                 Settings = setList
             };
-            
-            Models.Payloads.ZSSettingPayload.CreateOrUpdate(newRecord);
-            return Json(Models.Payloads.ZSSettingPayload.GetSettingsByUser("testdk"));
+            ZSSettingPayload.CreateOrUpdate(newRecord);
+
+            return Json(ZSSettingPayload.GetSettingsByUser(zmodId));
         }
         #endregion
+
+        #region GET Settings
+        [HttpGet]
+        public async Task<IActionResult> GetSettingsAsync()
+        {
+            //get the zmodId
+            string zmodId = "";// ZSSettingPayload.GetUserNameOrEmail(HttpContext);
+            await Task.FromResult(0);
+            var settings = ZSSettingPayload.GetSettingsByUser(zmodId);
+            // //mask uname and pass with *
+            // foreach (var record in settings)
+            // {
+            //     foreach (var s in record.Settings)
+            //     {
+            //         s.username = "********";
+            //         s.password = "********";
+            //     }
+            // }
+
+            if(settings.Count == 0)
+            {
+                
+                return Json(new List<SettingProperty>{
+                    new SettingProperty{ name="Cumulocity",type="C8Y",tenantID="ai", username="",password="",url="",selected=false },
+                    new SettingProperty{ name="Zementis Server",type="ZS",tenantID="ai", username="",password="",url="",selected=false }
+                });
+            }
+
+            return Json(settings);
+        }
+        #endregion
+
     }
 }
--- zmm/src/Models/Payloads/ZSSettingPayload.cs
+++ zmm/src/Models/Payloads/ZSSettingPayload.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using ZMM.Models.ResponseMessages;
 using ZMM.Models.Storage;
 
@@ -11,21 +12,20 @@ namespace ZMM.Models.Payloads
     {
         #region CreateOrUpdate
         public static ZSSettingResponse CreateOrUpdate(ZSSettingResponse newRecord)
-        {
+        {            
             var existingSettings = GetSettingsByUser(newRecord.ZmodId);
             ZSSettingResponse response = new ZSSettingResponse();
-            if(existingSettings.Count > 0)
+            if (existingSettings.Count > 0)
             {
-                //update
                 bool result = GlobalStorage.ZSSettingStorage.TryRemove(newRecord.ZmodId, out response);
-                if (result)  GlobalStorage.ZSSettingStorage.TryAdd(newRecord.ZmodId, newRecord);
+                if (result) GlobalStorage.ZSSettingStorage.TryAdd(newRecord.ZmodId, newRecord);
             }
             else
             {
                 //add new
-                GlobalStorage.ZSSettingStorage.TryAdd(newRecord.ZmodId, newRecord);  
+                GlobalStorage.ZSSettingStorage.TryAdd(newRecord.ZmodId, newRecord);
             }
-                      
+
             return newRecord;
         }
         #endregion
@@ -44,10 +44,40 @@ namespace ZMM.Models.Payloads
                     }
                 }
             }
-            
+
             return _settings;
         }
         #endregion
 
+        #region GetInfo - url,uname,pass
+        public static Tuple<string, string, string> GetUserInfo(string zmodId)
+        {
+            string url = "", uname = "", pass = "";
+            var qry = GetSettingsByUser(zmodId)
+            .Where(i => i.Settings.Any(s => s.selected == true))
+            .SelectMany(col => col.Settings.Select(s => new { s.url, s.username, s.password }));
+
+            foreach (var record in qry)
+            {
+                url=record.url;
+                uname=record.username;
+                pass=record.password;
+            }
+
+            return new Tuple<string, string, string>(url, uname, pass);
+        }
+        #endregion
+    
+        public static string GetUserNameOrEmail(HttpContext context)
+        {
+            Dictionary<string,string> Result = new Dictionary<string, string>();            
+            foreach(System.Security.Claims.Claim Cl in context.User.Claims)
+            {
+                if(Cl.Type.Equals("name")) Result["name"] = Cl.Value;
+                else if(Cl.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")) Result["email"] = Cl.Value;
+                else if(Cl.Type.Equals("role")) Result["role"] = Cl.Value;
+            }
+            return Result["email"];
+        }
     }
 }
