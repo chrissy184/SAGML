a3530bbe2bcbb4708765a9f4993d0dbf0c2d691d
Swapnil <swapnil.sharma@softwareag.com>
2019-12-23 14:39:05 +0530

    FIx for XGB classifier, XGB Reg and Upload to ZAD

--- zmk/nyokaBase/skl/skl_to_pmml.py
+++ zmk/nyokaBase/skl/skl_to_pmml.py
@@ -119,7 +119,7 @@ def model_to_pmml(toExportDict, PMMLFileName='from_sklearn.pmml',tyP=None):
                                             target_name,
                                             mining_imp_val,
                                             categoric_values,
-                                            tasktype)
+                                            tasktype,modelPath)
             
             elif ('LGBMRegressor' in str(model)) or ('LGBMClassifier' in str(model)):
                 PMML_kwargs = lgb_to_pmml(model,
@@ -128,7 +128,7 @@ def model_to_pmml(toExportDict, PMMLFileName='from_sklearn.pmml',tyP=None):
                                             target_name,
                                             mining_imp_val,
                                             categoric_values,
-                                            tasktype)                                
+                                            tasktype,modelPath)                                
             
             elif ('Booster' in str(model)):
                 PMML_kwargs = ext(model,tasktype,target_name)
