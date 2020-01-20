a6e9148cc12f6664bf9c192a0866822bba71d8b8
surbhi-software <53418666+surbhi-software@users.noreply.github.com>
2020-01-20 15:00:22 +0530

    Path traversal while uploading,modifying file=> ZENA-1062

--- zmm/src/Helpers/Common/FilePathHelper.cs
+++ zmm/src/Helpers/Common/FilePathHelper.cs
@@ -3,55 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ZMM.Models.ResponseMessages;
-
+using System.IO;
 namespace ZMM.Helpers.Common
 {
     public static class FilePathHelper
     {
+        private static readonly char[] InvalidFilenameChars = Path.GetInvalidFileNameChars();
         public static string GetFilePathById<T>(string id, List<T> records)
         {
-            string filePath = "";            
+            string filePath = "";
             IEnumerable<CodeResponse> _c = new List<CodeResponse>();
             IEnumerable<DataResponse> _d;
-            IEnumerable<ModelResponse> _m;            
+            IEnumerable<ModelResponse> _m;
 
-            if ((records == null) || (records.Count <= 0)) return "";            
+            if ((records == null) || (records.Count <= 0)) return "";
 
-            switch(typeof(T).Name)
-            {               
+            switch (typeof(T).Name)
+            {
                 case "CodeResponse":
-                    _c = records.Cast<CodeResponse>();                   
-                    foreach(var resp in _c)
-                    {			
-			            if(resp.Id == id)
+                    _c = records.Cast<CodeResponse>();
+                    foreach (var resp in _c)
+                    {
+                        if (resp.Id == id)
                         {
                             filePath = resp.FilePath;
                         }
-		            }
+                    }
                     break;
 
-                 case "DataResponse":
+                case "DataResponse":
                     _d = records.Cast<DataResponse>();
-                    foreach(var resp in _d)
-                    {			
-			            if(resp.Id == id)
+                    foreach (var resp in _d)
+                    {
+                        if (resp.Id == id)
                         {
                             filePath = resp.FilePath;
                         }
-		            }
+                    }
                     break;
                 case "ModelResponse":
-                    _m = records.Cast<ModelResponse>();                   
-                    foreach(var resp in _m)
-                    {			
-			            if(resp.Id == id)
+                    _m = records.Cast<ModelResponse>();
+                    foreach (var resp in _m)
+                    {
+                        if (resp.Id == id)
                         {
                             filePath = resp.FilePath;
                         }
-		            }
-                    break;                 
+                    }
+                    break;
             }
             return filePath;
+        }
+        public static bool IsFileNameValid(string FileName)
+        {
+            bool blnStatus = false;
+            if (FileName.IndexOfAny(InvalidFilenameChars) >= 0)
+               return blnStatus = false;
+            else
+                return blnStatus = true;
         }
     }
 }
