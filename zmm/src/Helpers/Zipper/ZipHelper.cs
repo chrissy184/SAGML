a0172f7206625f89eb836b65dd79047c33ce00e6
Nimesh <38841604+nimeshgit@users.noreply.github.com>
2020-01-22 16:26:07 +0530

    [Security Fixes] Resolve recursive zip bomb

--- zmm/src/Helpers/Zipper/ZipHelper.cs
+++ zmm/src/Helpers/Zipper/ZipHelper.cs
@@ -17,6 +17,8 @@ namespace ZMM.Helpers.Zipper
 
         private static readonly long MAXLIMITOFEXTRATEDFILESIZE = 2147483648;
 
+        private const int MAXIMUMNUMBEROFFILES = 1024;
+
         private static FileSystemWatcher DirectoryWatcher;
 
         #region Constructor
@@ -47,17 +49,21 @@ namespace ZMM.Helpers.Zipper
         private static bool SanitizeZipFile(string FilePath)
         {
             bool IsFileSanitized = true;
+            long TempSumOfVolume = 0;
+            int NumberOfFiles = 0;
             if(Path.GetExtension(FilePath).Contains("zip")) 
             {
                 using (ZipArchive archive = ZipFile.OpenRead(FilePath))
                 {
                     foreach (ZipArchiveEntry entry in archive.Entries)
                     {
+                        NumberOfFiles++;
+                        TempSumOfVolume += entry.Length; 
                         Console.WriteLine ("UnZipping file : " + FilePath + " Size : " + entry.Length); 
-                        if(entry.Length > MAXLIMITOFEXTRATEDFILESIZE) 
+                        if(entry.Length > MAXLIMITOFEXTRATEDFILESIZE || TempSumOfVolume > MAXLIMITOFEXTRATEDFILESIZE || NumberOfFiles > MAXIMUMNUMBEROFFILES) 
                         {
-                            throw new Exception("Zip file exceeds maximum size limit 2 GB. Please, upload zip file which has contents size less then 2 GB.");   
-                        }               
+                            throw new Exception("Zip file exceeds maximum size limit 2 GB or maximum number of contents items limit 1024. Please, upload zip file which has contents size less then 2 GB.");   
+                        }
                     }
                 } 
             }
