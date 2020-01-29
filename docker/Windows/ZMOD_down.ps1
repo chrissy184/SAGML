2d59eabc91ddbf45f4a92b6316e197b438ea1332
Nimesh <38841604+nimeshgit@users.noreply.github.com>
2020-01-29 23:09:36 +0530

    refactor folder
    removed unwanted files

--- docker/Windows/ZMOD_down.ps1
+++
@@ -1,4 +0,0 @@
-docker stop zmk1 zmk2 zmk3
-docker-compose down
-
-
--- docker/Windows/ZMOD_up.ps1
+++
@@ -1,6 +0,0 @@
-
-docker-compose up -d
-docker exec -it nginx_zmod bash -c 'rm nginx/conf.d/default.conf; service nginx restart'
-docker exec -it zmm bash -c 'echo "  inside zmm" && echo alias nyoka="/publish/nyokacli/client/bin/release/netcoreapp2.1/linux-x64/publish/nyoka" >> ~/.bashrc && echo "DONE"'
-
-
