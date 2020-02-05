f5a06d6dec0ffec9fc6453c7db766886819efe0c
Swapnil <swapnil.sharma@softwareag.com>
2020-02-05 11:40:06 +0530

    Update and fix for MRCNN

--- zmk/trainModel/mrcnn/visualize.py
+++ zmk/trainModel/mrcnn/visualize.py
@@ -162,10 +162,10 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             verts = np.fliplr(verts) - 1
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
-    ax.imshow(masked_image.astype(np.uint8))
+    # ax.imshow(masked_image.astype(np.uint8))
     # if auto_show:
     #     plt.show()
-    return ax
+    return masked_image.astype(np.uint8)
 
 
 def display_differences(image,
