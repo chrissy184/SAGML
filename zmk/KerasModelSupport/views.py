0415e5d38141f1a3897779bc9f1ef215af89d54e
Swapnil <swapnil.sharma@softwareag.com>
2020-04-03 12:58:02 +0530

    Update for ONNX scoring

--- zmk/KerasModelSupport/views.py
+++ zmk/KerasModelSupport/views.py
@@ -4,6 +4,12 @@ from django.http import JsonResponse
 import pathlib,datetime
 from tensorflow import Graph, Session
 import tensorflow as tf
+from string import ascii_uppercase
+from random import choice
+import os,json
+import numpy as np
+from keras.preprocessing import image
+import pandas as pd
 
 from trainModel.kerasUtilities import PMMLMODELSTORAGE
 global PMMLMODELSTORAGE
@@ -96,6 +102,17 @@ class KerasExecution:
 
 class ONNXExecution:
 
+    def checkCreatePath(self,folderPath):
+        try:
+            if os.path.exists(folderPath):
+                return ('path exist')
+            else:
+                os.makedirs(folderPath)
+                return ('path created')
+        except:
+            os.makedirs(folderPath)
+            return ('path created')
+
     def loadOnnxModel(self,filePath):
         print ('Came here')
         global PMMLMODELSTORAGE
@@ -184,5 +201,48 @@ class ONNXExecution:
         allInfo['information'].append(tempDict)
         return JsonResponse(allInfo)
 
-    def scoreOnnxModel(self,modelKey):
-        return None
+    def scoreOnnxModel(self,modelKey,filePath):
+        import numpy as np
+        print ('came here')
+        print ('came here',modelKey,filePath)
+        target_path='./logs/'+''.join(choice(ascii_uppercase) for i in range(12))+'/'
+        self.checkCreatePath(target_path)
+        global PMMLMODELSTORAGE
+        # print (PMMLMODELSTORAGE[modelName])
+        modelInformation =PMMLMODELSTORAGE[modelKey]
+        print (modelInformation)
+        modelObjs=list(modelInformation.keys())
+        print (modelObjs)
+        sess=modelInformation['model_session']
+        output_name = sess.get_outputs()[0].name
+
+        if pathlib.Path(filePath).suffix =='.csv':
+            testData=pd.read_csv(filePath)
+        elif pathlib.Path(filePath).suffix in ['.jpg','.JPG','.png','.PNG']:
+            inputShapevals=modelInformation['inputShape'][0]
+            testimage=filePath
+            img_height, img_width=inputShapevals[1:3]
+            img = image.load_img(testimage, target_size=(img_height, img_width))
+            testData = image.img_to_array(img)
+            testData = np.expand_dims(testData, axis=0)
+            testData=testData/255
+            testData = testData if isinstance(testData, list) else [testData]
+            inputForModel=[(ob.name,testData[en]) for en,ob in enumerate(sess.get_inputs())]
+            res = sess.run([output_name], dict(inputForModel))
+            predi=res[0]
+
+            if pathlib.Path(filePath).suffix =='.csv':
+                testData['predicted_Score']=predi
+                print (testData.shape)
+                resafile=target_path+'result.csv'
+                testData.to_csv(resafile, index=False)
+            elif pathlib.Path(filePath).suffix in ['.jpg','.JPG','.png','.PNG']:
+                import numpy as np
+                predClasses=['class_'+str(i) for i in range(len(np.ravel(predi)))]
+                targetResult= {j:str(float(k)) for j,k in zip(predClasses,list(predi[0]))}
+                resafile=target_path+'result.txt'
+                with open(resafile,'w') as f:
+                    f.write(json.dumps(targetResult))
+
+        resultResp={'result':resafile}
+        return JsonResponse(resultResp,status=200)
